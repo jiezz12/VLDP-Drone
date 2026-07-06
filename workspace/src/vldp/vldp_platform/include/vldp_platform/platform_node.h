3385a04c828fa72daabcbf000e5691c817cde2ad
jiezz12 <a2499420178@163.com>
2026-07-06 22:52:21 +0800

    feat(platform): 新增Camera订阅及Heartbeat状态检测

--- workspace/src/vldp/vldp_platform/include/vldp_platform/platform_node.h
+++ workspace/src/vldp/vldp_platform/include/vldp_platform/platform_node.h
@@ -6,6 +6,11 @@
  */
 
 #include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include <opencv2/opencv.hpp>
 
 namespace vldp
 {
@@ -27,7 +32,55 @@ public:
 
 private:
 
+  /*-------------------------------
+     * ROS
+     *------------------------------*/
     ros::NodeHandle nh_;
+
+    image_transport::ImageTransport it_;
+
+    image_transport::Subscriber image_sub_;
+
+    ros::Timer status_timer_;
+
+    /*-------------------------------
+     * Camera
+     *------------------------------*/
+    cv::Mat latest_image_;
+
+    uint64_t image_count_{0};
+
+	/* 最近一次收到图像的时间 */
+	ros::Time last_image_time_;
+
+	/* 在线状态 */
+	bool camera_online_{false};
+
+	/* 每秒统计 */
+	uint32_t image_counter_{0};
+
+	double camera_fps_{0.0};
+
+        uint32_t status_tick_{0};
+
+	/* 超时时间 */
+	double camera_timeout_{2.0};
+
+    /*-------------------------------
+     * Callback
+     *------------------------------*/
+    void ImageCallback(
+        const sensor_msgs::ImageConstPtr& msg);
+
+    void StatusTimerCallback(
+        const ros::TimerEvent& event);
+
+    /*-------------------------------
+     * Status
+     *------------------------------*/
+    bool IsCameraOnline() const;
+
+    void PrintStatus();
 };
 
 } // namespace vldp
--- workspace/src/vldp/vldp_platform/src/platform_node.cpp
+++ workspace/src/vldp/vldp_platform/src/platform_node.cpp
@@ -2,20 +2,136 @@
 
 #include "vldp_common/logger.h"
 
+#include <sstream>
+
 namespace vldp
 {
 
 PlatformNode::PlatformNode(ros::NodeHandle& nh)
     : nh_(nh)
+    
+    , it_(nh_)
 {
 
 }
 
 bool PlatformNode::Initialize()
 {
-    Log::Info("Platform 初始化成功");
+      image_sub_ = it_.subscribe(
+        "/iris/stereo_camera/left/image_raw",
+        1,
+        &PlatformNode::ImageCallback,
+        this);
+
+    status_timer_ = nh_.createTimer(
+        ros::Duration(1.0),
+        &PlatformNode::StatusTimerCallback,
+        this);
+
+    Log::Info("Platform 初始化完成");
+    Log::Info("Camera Subscriber 建立");
 
     return true;
+}
+
+void PlatformNode::ImageCallback(
+    const sensor_msgs::ImageConstPtr& msg)
+{
+    try
+    {
+    /*
+        latest_image_ = cv_bridge::toCvCopy(
+
+            msg,
+
+            sensor_msgs::image_encodings::BGR8
+
+        )->image;
+*/
+	latest_image_ = cv_bridge::toCvShare(msg, msg->encoding)->image.clone();
+	
+	last_image_time_=ros::Time::now();
+	
+        image_count_++;
+
+	image_counter_++;
+
+    }
+    catch(const cv_bridge::Exception& e)
+    {
+        Log::Error(e.what());
+    }
+}
+
+bool PlatformNode::IsCameraOnline() const
+{
+    if(last_image_time_.isZero())
+    {
+        return false;
+    }
+
+    return
+        (ros::Time::now()-last_image_time_).toSec()
+        <
+        camera_timeout_;
+}
+
+void PlatformNode::PrintStatus()
+{
+    std::stringstream ss;
+
+    ss << "\n========== VLDP Platform ==========\n";
+
+/*
+    ss << "Camera : ";
+
+    if(IsCameraOnline())
+    {
+        ss << "OK";
+    }
+    else
+    {
+        ss << "Waiting";
+    }
+
+    ss << "\n";
+*/
+    ss << "Camera : " << (IsCameraOnline() ? "OK" : "Waiting");
+
+    Log::Info(ss.str());
+}
+
+void PlatformNode::StatusTimerCallback(
+    const ros::TimerEvent&)
+{
+    bool online = IsCameraOnline();
+
+    if(online != camera_online_)
+    {
+        camera_online_ = online;
+
+        if(camera_online_)
+        {
+            Log::Info("Camera Connected");
+        }
+        else
+        {
+            Log::Warn("Camera Lost");
+        }
+        
+    }
+
+    camera_fps_ = static_cast<double>(image_counter_);
+
+    image_counter_ = 0;
+
+    status_tick_++;
+	if (camera_online_ && status_tick_ % 5 == 0)
+	{
+	    std::ostringstream ss;
+	    ss << "Camera FPS: " << camera_fps_;
+	    Log::Info(ss.str());
+	}
 }
 
 } // namespace vldp
